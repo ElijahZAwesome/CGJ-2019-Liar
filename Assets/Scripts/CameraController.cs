4c3007e6b771dffa1ccd5ebb7f381e80a23bb606
ElijahZAwesome <>
2019-08-26 15:39:43 -0500

    More camera movement and Prefabs folder

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// This script manages the camera's movement and position in the scene.
+    /// </summary>
+
+    // 0 is left, 1 is center, 3 is right
+    private int cameraPosition = 1;
+
+    public Transform[] cameraPositions;
+
+    public void MoveCam(int position)
     {
-        
+        // Stub
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if(!Input.GetKeyUp(KeyCode.D) && !Input.GetKeyUp(KeyCode.A))
+        {
+            cameraPosition = 1;
+        }
+
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            cameraPosition = 3;
+        }
     }
 }
