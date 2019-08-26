3877b960c3e753d39968d6b73dd317ef49f79de2
zombykillr123 <jaklein25@gmail.com>
2019-08-26 15:07:06 -0500

    Added scripts folder and test scene
    Added the test scene for testing logic, and the scripts folder

---
+++ Assets/Scripts/MasterLogic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterLogic : MonoBehaviour
+{
+    /// <summary>
+    /// Handles the overall GameLogic of which door ends up being safe
+    /// </summary>
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
