db5ff00fbf734fa2d329da2f8e030d88bf8ab5fd
ElijahZAwesome <>
2019-08-26 23:25:30 -0500

    Rulebook pseudocode

--- Assets/Scripts/RuleBook.cs
+++ Assets/Scripts/RuleBook.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class RuleBook : MonoBehaviour
@@ -7,8 +8,53 @@ public class RuleBook : MonoBehaviour
     // The grand variable, that in the end, deterines if the sign is lying
     private bool signIsLying;
 
+    // Delegate so we can store methods
+    public delegate bool RuleMethod();
+
     // Which rules are in this game
     public List<int> rulesInThisGame;
+
+    // Rulebook containing our rules
+    public List<RuleMethod> rules = new List<RuleMethod>();
+
+    // Add each rule that is defined at the bottom of this script. Use this as an example
+    public RuleBook()
+    {
+        rules.Add(LyingIfRat);
+    }
+
+    // Randomly generates 2-4 rules to start
+    public void StartRules()
+    {
+        int howMany = Mathf.RoundToInt(UnityEngine.Random.Range(2, 4));
+        for (int i = 0; i < howMany; i++)
+        {
+            rulesInThisGame.Add(Mathf.RoundToInt(UnityEngine.Random.Range(0, rules.Count)));
+        }
+
+    }
+
+    /// <summary>
+    /// Calls each rule, which will modify signIsLying and also return whether or not the rule was true
+    /// (currently i can't think of a use for this, but futureproofing never hurts)
+    /// </summary>
+    public void RunThroughRules()
+    {
+        rulesInThisGame.ForEach((t) =>
+        {
+            rules[t]();
+        });
+    }
+
+    /// <summary>
+    /// If rat, sign is lying (always true, for pseudocode purposes)
+    /// </summary>
+    public bool LyingIfRat()
+    {
+        bool rat = true;
+        signIsLying = rat;
+        return rat;
+    }
 
     /* Example:
      * Rules in this game: 1, 3, 5, 6
@@ -20,15 +66,4 @@ public class RuleBook : MonoBehaviour
      * Functions should be short, as there are many
      */
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
