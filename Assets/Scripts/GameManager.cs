57338a800918b1a6831d335036f8d21b68d0e8e8
ZAG852 <36207290+ZAG852@users.noreply.github.com>
2019-08-27 13:33:26 -0500

    GameManger Update
    It should now generate a key

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public static GameManager instance;
+	//Records corner of map which will be split off from and used to get the total size of the room so you can place things at (x,y)
+	[SerializeField]
+	private GameObject cornerOfMap;
+	//Rocks V Stalactites V etc....
+	private string key = "0v0";
+	//Add more lists of the objects as needed to be spawned
+	private List<GameObject> rocks;
+	private List<GameObject> stalactites;
+	//This controlls the number of things the key will be able to generate. Decided by list of things like rocks and stalactites
+	int numberOfThings = 2, difficultyLevels = 5;
+	// Start is called before the first frame update
+	void Start()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+	}
+	//call this on the completion of a room to update the key
+	void KeyUpdate()
+	{
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		List<int> keyPart = new List<int>();
+		for (int i = 0; i < numberOfThings; i++)
+		{
+
+			keyPart.Add(int.Parse(key.Split('v')[i]) + difficultyLevels);
+		}
+		//Add to key based on where the object you want to influene is in the list based on what you want the difficulty to be and how the scale will rise.
+		key = keyPart[0].ToString() + 'v' + keyPart[1].ToString();
+
+	}
+	/**
+	void GenTheRoom()
+	{
+
+	}
+	**/
 }
