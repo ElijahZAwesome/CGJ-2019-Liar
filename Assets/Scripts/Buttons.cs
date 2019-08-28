94804965f3ded84ac27b8a2d6bffcf5dac556105
WesMcW <wesmcwhorter@gmail.com>
2019-08-28 13:54:01 -0500

    Updated title

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    public GameObject howToPlay;
+
+    void Start()
+    {
+        howToPlay.SetActive(false);
+    }
+
+    public void openRules()
+    {
+        howToPlay.SetActive(true);
+    }
+
+    public void closeRules()
+    {
+        howToPlay.SetActive(false);
+    }
+
+    public void loadGame()
+    {
+        SceneManager.LoadScene("OtherSampleScene");
+    }
+
+    public void exitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
