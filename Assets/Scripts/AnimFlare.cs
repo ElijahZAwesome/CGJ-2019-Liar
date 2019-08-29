adc2a9776792f22b630c0eedb2b613606858008d
Lebrra <Lebrra@users.noreply.github.com>
2019-08-28 22:59:10 -0500

    New cave light flare thing

---
+++ Assets/Scripts/AnimFlare.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimFlare : MonoBehaviour
+{
+    public bool correct = false;
+    public GameObject caveLight;
+
+    void Start()
+    {
+        caveLight = transform.GetChild(0).gameObject;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void stopAnim()
+    {
+        Destroy(gameObject);
+    }
+
+    void addLight()
+    {
+        if (correct)
+        {
+            caveLight.transform.parent = null;
+            caveLight.SetActive(true);
+        }
+    }
+}
