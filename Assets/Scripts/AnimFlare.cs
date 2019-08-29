0012cdaafd13b8cac2c0c5e987f7a966fac7b91e
Lebrra <Lebrra@users.noreply.github.com>
2019-08-29 10:00:00 -0500

    throwing a flare will leave a check or x if its right or wrong (still needs to connect to master logic though)

--- Assets/Scripts/AnimFlare.cs
+++ Assets/Scripts/AnimFlare.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class AnimFlare : MonoBehaviour
 {
+    public bool flare;
     public bool correct = false;
     public GameObject caveLight;
+    public GameObject x;
+    public GameObject o;
 
     void Start()
     {
@@ -19,20 +22,24 @@ public class AnimFlare : MonoBehaviour
 
     void stopAnim()
     {
+        if (flare)
+        {
+            if (correct)
+            {
+                // put check on door
+                Instantiate(o, transform.position, Quaternion.identity);
+            }
+            else
+            {
+                // put X on door
+                Instantiate(x, transform.position, Quaternion.identity);
+            }
+        }
         Destroy(gameObject);
     }
 
     void addLight()
     {
         Instantiate(caveLight, gameObject.transform.position, Quaternion.identity);
-
-        if (correct)
-        {
-            // put check on door
-        }
-        else
-        {
-            // put X on door
-        }
     }
 }
